b5c92ab46ba1f67e7ed5f63791c86ca5a53d37dc
jacoblin88 <jacoblin88@gmail.com>
2022-04-11 12:00:05 +0800

    Add AD forest automatically

--- powershell/DCSetting/EnableADService.ps1
+++ powershell/DCSetting/EnableADService.ps1
@@ -1,2 +1,22 @@
 Install-WindowsFeature -Name AD-Domain-Services -IncludeManagementTools
+<#
+enable DNS Plugin
+#>
+<#
+$pass = ConvertTo-SecureString -string 'password' -AsPlainText -Force
+#>
 
+Import-Module ADDSDeployment
+Install-ADDSForest `
+-DomainName "atop.com.tw"
+-CreateDnsDelegation:$false `
+-DatabasePath "C:\\Windows\NTDS" `
+-DomainMode "WinThreshold" `
+-DomainNetbiosName "atop" `
+-ForestMode "WinThreshold" `
+-InstallDns:$true`
+-LogPath "C:\Windows\NTDS"
+-NoRebootOnCompletion: $false `
+-SysvolPath "C:\\Windows\SYSVOL" `
+-SafeModeAdministratorPassword $pass `
+-Force:$True
