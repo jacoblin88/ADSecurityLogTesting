081c10d846b879a6fc15e2fed5f10be294fee01f
jacoblin88 <jacoblin88@gmail.com>
2022-04-12 16:16:28 +0800

    update

--- powershell/DCSetting/6-2_AddComputerToOU_RD.ps1
+++ powershell/DCSetting/6-2_AddComputerToOU_RD.ps1
@@ -1,3 +1,3 @@
-$SecPassword = ConvertTo-SecureString "P@ssw0rdcom_add" -AsPlainText -Force
+$SecPassword = ConvertTo-SecureString "com1qaz@WSXadd" -AsPlainText -Force
 $Cred = New-Object System.Management.Automation.PSCredential("atop\com_add",$SecPassword)
 Add-Computer -DomainName atop.com.tw -Credential $Cred -OUPath "ou=RD Dept,dc=atop,dc=com,dc=tw" -Restart -force -newname web01
--- powershell/DCSetting/6-3_AddComputerToOU_CS.ps1
+++ powershell/DCSetting/6-3_AddComputerToOU_CS.ps1
@@ -1,4 +1,4 @@
-$SecPassword = ConvertTo-SecureString "P@ssw0rdcom_add" -AsPlainText -Force
+$SecPassword = ConvertTo-SecureString "com1qaz@WSXadd" -AsPlainText -Force
 $Cred = New-Object System.Management.Automation.PSCredential("atop\com_add",$SecPassword)
 Add-Computer -DomainName atop.com.tw -Credential $Cred -OUPath "ou=CS Dept,dc=atop,dc=com,dc=tw" -Restart -force -newname exchange01
 
