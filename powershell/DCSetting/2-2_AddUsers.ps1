2131cb83b5d42a74a583adde7f406b0c38105ac7
jacoblin88 <jacoblin88@gmail.com>
2022-04-12 13:50:40 +0800

    modify password

--- powershell/DCSetting/2-2_AddUsers.ps1
+++ powershell/DCSetting/2-2_AddUsers.ps1
@@ -1,20 +1,20 @@
-$pw = ConvertTo-SecureString "P@ssw0rdITadmin" -AsPlainText -Force
+$pw = ConvertTo-SecureString "IT1qaz@WSXadmin" -AsPlainText -Force
 New-ADUser -Name "ITadmin" -SamAccountName "ITadmin" -AccountPassword $pw -Enabled $true -Path "ou=IT Dept,dc=atop,dc=com,dc=tw"
 
-$pw = ConvertTo-SecureString "P@ssw0rdITuser" -AsPlainText -Force
+$pw = ConvertTo-SecureString "IT1qaz@WSXuser" -AsPlainText -Force
 New-ADUser -Name "ITuser" -SamAccountName "ITuser" -AccountPassword $pw -Enabled $true -Path "ou=IT Dept,dc=atop,dc=com,dc=tw"
 
-$pw = ConvertTo-SecureString "P@ssw0rdcom_add" -AsPlainText -Force
+$pw = ConvertTo-SecureString "com1qaz@WSXadd" -AsPlainText -Force
 New-ADUser -Name "com_add" -SamAccountName "com_add" -AccountPassword $pw -Enabled $true -Path "ou=IT Dept,dc=atop,dc=com,dc=tw"
 
-$pw = ConvertTo-SecureString "P@ssw0rdRDadmin" -AsPlainText -Force
+$pw = ConvertTo-SecureString "RD1qaz@WSXadmin" -AsPlainText -Force
 New-ADUser -Name "RDadmin" -SamAccountName "RDadmin" -AccountPassword $pw -Enabled $true -Path "ou=RD Dept,dc=atop,dc=com,dc=tw"
 
-$pw = ConvertTo-SecureString "P@ssw0rdRDuser" -AsPlainText -Force
+$pw = ConvertTo-SecureString "RD1qaz@WSXuser" -AsPlainText -Force
 New-ADUser -Name "RDuser" -SamAccountName "RDuser" -AccountPassword $pw -Enabled $true -Path "ou=RD Dept,dc=atop,dc=com,dc=tw"
 
-$pw = ConvertTo-SecureString "P@ssw0rdCSadmin" -AsPlainText -Force
+$pw = ConvertTo-SecureString "CS1qaz@WSXadmin" -AsPlainText -Force
 New-ADUser -Name "CSadmin" -SamAccountName "CSadmin" -AccountPassword $pw -Enabled $true -Path "ou=CS Dept,dc=atop,dc=com,dc=tw"
 
-$pw = ConvertTo-SecureString "P@ssw0rdCSuser" -AsPlainText -Force
+$pw = ConvertTo-SecureString "CS1qaz@WSXuser" -AsPlainText -Force
 New-ADUser -Name "CSuser" -SamAccountName "CSuser" -AccountPassword $pw -Enabled $true -Path "ou=CS Dept,dc=atop,dc=com,dc=tw"
