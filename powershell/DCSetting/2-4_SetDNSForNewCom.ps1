95a379817649db32e50f392a06f714577f3e1d2e
jacoblin88 <jacoblin88@gmail.com>
2022-04-12 10:11:54 +0800

    AnotherChanges

---
+++ powershell/DCSetting/2-4_SetDNSForNewCom.ps1
@@ -0,0 +1,7 @@
+<#
+Set DC IP as our dns ip
+#>
+$IntId = Get-NetIPAddress -ipaddress 10.0.1.* | Select-Object -Property InterfaceIndex
+$IntId = $IntId."InterfaceIndex"
+$DCIP = "10.0.1.104"
+Set-DnsClientDohServerAddress -InterfaceIndex $IntId -ServerAddress($DCIP,"8.8.8.8")
