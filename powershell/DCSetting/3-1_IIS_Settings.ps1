5a5f05fc46944ed9192e4f7638680ba9d22b4a26
jacoblin88 <jacoblin88@gmail.com>
2022-04-12 17:35:28 +0800

    Final Version

--- powershell/DCSetting/3-1_IIS_Settings.ps1
+++ powershell/DCSetting/3-1_IIS_Settings.ps1
@@ -9,3 +9,6 @@ New-Website -name vulnweb -PhysicalPath C:\\inetpub\vulnweb -ApplicationPool "vu
 
 Set-WebConfigurationProperty -Filter /system.webServer/security/authentication/anonymousAuthentication -Name enabled -Value false -PSPath IIS:\ -location 'vulnweb'
 Set-WebConfigurationProperty -filter /system.webServer/security/authentication/windowsAuthentication -Name enabled -Value true -PSPath IIS:\ -Location 'vulnweb'
+
+Set-ItemProperty -Path "IIS:\\sites\vulnweb" -Name "bindings" -Value @{protocol="http";bindinginformation="*:8080:"}
+Get-Website -name vulnweb | Start-Website
