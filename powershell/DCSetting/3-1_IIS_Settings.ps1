7be0d3ec441d123b2eb3070ebb0154869253f921
jacoblin88 <jacoblin88@gmail.com>
2022-04-12 17:24:10 +0800

    correct

--- powershell/DCSetting/3-1_IIS_Settings.ps1
+++ powershell/DCSetting/3-1_IIS_Settings.ps1
@@ -4,7 +4,8 @@ Enable-WindowsOptionalFeature -Online -FeatureName IIS-WindowsAuthentication
 Import-Module WebAdministration
 New-Item -Path IIS:\\AppPools\vulnweb
 
-New-Website -name vulnweb -PhysicalPath C:\\inetpub\vulnweb -ApplicationPool "IIS:\\AppPools\vulnweb"
+mkdir -p C:\\inetpub\vulnweb
+New-Website -name vulnweb -PhysicalPath C:\\inetpub\vulnweb -ApplicationPool "vulnweb"
 
-Set-WebConfigurationProperty -Filter /system.webServer/security/authentication/anonymousAuthentication -Name enabled -Value false -PSPath IIS:\ -location 'Default Web Site/web01'
-Set-WebConfigurationProperty -filter /system.webServer/security/authentication/windowsAuthentication -Name enabled -Value true -PSPath IIS:\ -Location 'Default Web Site/web01'
+Set-WebConfigurationProperty -Filter /system.webServer/security/authentication/anonymousAuthentication -Name enabled -Value false -PSPath IIS:\ -location 'vulnweb'
+Set-WebConfigurationProperty -filter /system.webServer/security/authentication/windowsAuthentication -Name enabled -Value true -PSPath IIS:\ -Location 'vulnweb'
